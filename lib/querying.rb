b76ce1ea5ec60dfb4b06715b2494f7a6874dd9a8
Diego Vasquez <botellongo69@outlook.com>
2020-02-19 17:31:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,4 +1,4 @@
-  def select_books_titles_and_years_in_first_series_order_by_year
+def select_books_titles_and_years_in_first_series_order_by_year
   <<-SQL
     SELECT title, year 
     FROM books 
