d43c15d29722d4e3aaed927f854c3dd3c3664466
Diego Vasquez <botellongo69@outlook.com>
2020-02-19 17:30:17 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,15 @@
-INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "white", "angry", 1);
-INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
-INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 0);
-INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1);
-INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
-INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
-INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "naive", 0);
-INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "The Red Book", 1, 1), (2, "SECOND EDITION", 2, 2);
+
+INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
+
+INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
+
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
+
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1), (2, "Tyrion Lanister", "A Lanister always pays is debts", "human", 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1), (4, "Eddard Stark", "Winter is coming", "human", 1);
+
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Character One", "motto one", "cylon", 2), (6, "Character Two", "motto two", "human", 2), (7, "Character Three", "motto three", "cylon", 2), (8, "Character Four", "motto four", "cylon", 2);
+
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
+
+INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
